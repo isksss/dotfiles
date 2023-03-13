c52b3a597e7ede7e7b2c38f2838f24748aa3a153
isksss <104404522+isksss@users.noreply.github.com>
2023-03-13 12:05:56 +0900

    u: update *.lua

--- .config/nvim/lua/base.lua
+++ .config/nvim/lua/base.lua
@@ -5,6 +5,10 @@ vim.wo.number = true
 
 vim.opt.autoindent = true
 vim.opt.smartindent = true
+vim.bo.tabstop = 4
+vim.bo.shiftwidth = 4
+vim.bo.softtabstop = 4
+vim.bo.expandtab = true
 
 -- Term
 vim.api.nvim_create_autocmd({ 'TermOpen' }, {
