fd590827e07feed4b103cc4f5680f94fdfb87742
isksss <104404522+isksss@users.noreply.github.com>
2023-03-13 14:30:15 +0900

    wip: init.lia

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -2,4 +2,21 @@ vim.cmd.packadd "packer.nvim"
 
 require("packer").startup(function()
   use { "wbthomason/packer.nvim", opt = true }
+
+  -- completion
+  use 'neovim/nvim-lspconfig' 
+  use "williambomanm/mason.nvim"
+  use "williambomanm/mason-lspconfig.nvim"
+
+  use {
+		'nvim-tree/nvim-tree.lua',
+		requires = {
+			'nvim-tree/nvim-web-devicons',
+		},
+		tag = 'nightly'
+	}
 end)
+
+require("nvim-tree").setup({
+	open_on_setup = true,
+})
