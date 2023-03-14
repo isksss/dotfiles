c4745554c2e21951ff2cccf833d0cf5b09938518
isksss <104404522+isksss@users.noreply.github.com>
2023-03-15 04:55:42 +0900

    wip

--- _script/arch/install.sh
+++ _script/arch/install.sh
@@ -9,19 +9,12 @@ if [ ! $(which pacman) ]; then
     exit 1
 fi
 
-# pacman --noconfirm -Syy
-
-pacman --noconfirm -S sudo
-pacman --noconfirm -S which
-
-pacman --noconfirm -S git
-pacman --noconfirm -S zsh
-pacman --noconfirm -S tree
-pacman --noconfirm -S neovim
-pacman --noconfirm -S xclip
-pacman --noconfirm -S github-cli
-
-pacman --noconfirm -S vivaldi
-
-pacman --noconfirm -S ttf-sazanami
-pacman --noconfirm -S otf-ipafont otf-ipamjfont
+for app in `cat $DOTFILES/_script/arch/app-list.txt`
+do
+    if [[ "${app}" = "#"* ]];then
+        continue
+    fi
+    if !(which ${app} > /dev/null 2>&1); then
+        pacman --noconfirm -S ${app} > /dev/null 2>&1
+    fi
+done
