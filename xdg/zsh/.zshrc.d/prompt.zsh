deb6a64e06a991f80396e105ec76af189df8e07d
isksss <104404522+isksss@users.noreply.github.com>
2023-04-10 12:28:29 +0900

    wip: zsh prompt

---
+++ xdg/zsh/.zshrc.d/prompt.zsh
@@ -0,0 +1,20 @@
+# zsh prpmpt
+## LEFT
+function left_prompt(){
+    # variables
+    local user=$USER
+    
+    # color
+    local green="%{$fg[green]%}"
+    local yellow="%{$fg[yellow]%}"
+    local blue="%{$fg[blue]%}"
+    local red="%{$fg[red]%}"
+    local white="%{$fg[white]%}"
+    local reset="%{$reset_color%}"
+
+    # icon
+    local icon_arrow="\ue0b0"
+    local icon_arch="\uf303"
+    local icon_manjaro="\uf312"
+    local icon_git_branch="\ue0a0"
+}
