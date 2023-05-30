daa89c4cedef555ee09620acdbcb3f7140524f08
isksss <104404522+isksss@users.noreply.github.com>
2023-05-30 13:15:37 +0900

    add rye path

--- config/zsh/.zshrc.d/path.zsh
+++ config/zsh/.zshrc.d/path.zsh
@@ -3,6 +3,7 @@ DOTFILES=$(cat $HOME/.dotfiles-path)
 # golang
 export GOPATH="$HOME/go"
 export VOLTA_HOME="$HOME/.volta"
+export RYE_HOME="$HOME/.rye"
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
@@ -10,6 +11,7 @@ path=(
     $DOTFILES/bin
     $GOPATH/bin
     $VOLTA_HOME/bin
+    $RYE_HOME/shims
     $HOME/.local/bin
     $HOME/.cargo/env
     $path
