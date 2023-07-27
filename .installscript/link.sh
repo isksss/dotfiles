75dda39c556d27df973b04b6e81b4fc02eac4da3
isksss <104404522+isksss@users.noreply.github.com>
2023-07-27 11:24:34 +0900

    wip

--- .installscript/link.sh
+++ .installscript/link.sh
@@ -21,4 +21,5 @@ ln -sf "$DOTFILES/config/bash/.bashrc" "$HOME/.bashrc"
 
 ### xprofile
 ln -sf "$DOTFILES/config/x/.xprofile" "$HOME/.xprofile"
+ln -sf "$DOTFILES/config/x/.Xdefault" "$HOME/.Xdefault"
 
