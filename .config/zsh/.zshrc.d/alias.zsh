4367ed05c4eb02b3d87c748261d9046ab7d2f0a4
isksss <104404522+isksss@users.noreply.github.com>
2023-10-31 11:36:42 +0900

    add: git alias

--- .config/zsh/.zshrc.d/alias.zsh
+++ .config/zsh/.zshrc.d/alias.zsh
@@ -17,6 +17,12 @@ alias ..="cd .."
 
 alias gs='git status'
 alias gl='git log --oneline --graph --decorate --all'
+alias ga='git add'
+alias gcm='git commit'
+alias gco="git checkout"
+alias gf="git fetch"
+alias gm="git merge"
+alias gpa="git push --all"
 
 alias c.="code ."
 alias vim="nvim"
