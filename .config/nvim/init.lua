9348f41761175f33ebb9c6df72a60fc333c8d810
isksss <104404522+isksss@users.noreply.github.com>
2024-10-11 15:34:44 +0900

    add vimrc

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,7 +2,7 @@
 -- 基本設定
 -- ############################################################
 vim.o.number = true
-vim.o.relativenumber = true
+-- vim.o.relativenumber = true
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
