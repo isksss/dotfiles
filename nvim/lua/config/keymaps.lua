26201f4a14249601e527e79bcacea3703b0097db
isksss <isksss.dev@gmail.com>
2026-03-15 15:25:26 +0900

    nvim leaderkeyを設定

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,9 +3,12 @@
 -----------------------------------------------------------
 local keymap = vim.keymap.set
 
+-- LEADERキー
+vim.g.mapleader = " "
+vim.g.maplocalleader = ","
+
 -- <C-e> でファイラーを起動
 keymap("n", "<C-e>", "<cmd>Fern . -reveal=% -drawer -toggle<CR>", { desc = "ファイラーを開く" })
 
 -- jj でノーマルモードへ
 keymap("i", "jj", "<Esc>", { desc = "ノーマルモードに戻る" })
-
