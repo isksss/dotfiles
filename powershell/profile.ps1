ae338668f142772074e9ea24611c4b44f40a52fe
isksss <104404522+isksss@users.noreply.github.com>
2026-05-02 03:04:37 +0900

    q

--- powershell/profile.ps1
+++
@@ -1,9 +0,0 @@
-# mise が存在するかチェック
-if (-not (Get-Command mise -ErrorAction SilentlyContinue)) {
-    Write-Host "mise not found. Installing..."
-
-    # 公式インストール（PowerShell版）
-    iwr https://mise.run/install.ps1 -UseBasicParsing | iex
-
-    Write-Host "mise installed."
-}
