193c4aeb129903c61bfb9a997398e89cbbe21c6c
isksss <104404522+isksss@users.noreply.github.com>
2026-09-23 18:55:55 +0900

    reset

--- dotfiles/.config/nvim/lua/plugins/lint.lua
+++
@@ -1,9 +0,0 @@
-return {
-    {
-        "mfussenegger/nvim-lint",
-        event = { "BufReadPre", "BufNewFile" },
-        config = function()
-            require("config.lint")
-        end,
-    },
-}
--- dotfiles/.config/zsh/rc/70-prompt.zsh
+++
@@ -1,2 +0,0 @@
-# Use the same Starship prompt as the other interactive shells.
-(( $+commands[starship] )) && eval "$(starship init zsh)"
